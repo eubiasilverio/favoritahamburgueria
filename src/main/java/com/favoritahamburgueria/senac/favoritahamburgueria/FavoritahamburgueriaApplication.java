2b245d9327c53a028ba777705dbd7e461de3868f
beatris.sbrigante <beatris_silverio@outlook.com>
2024-03-05 21:21:56 -0300

    projeto

---
+++ src/main/java/com/favoritahamburgueria/senac/favoritahamburgueria/FavoritahamburgueriaApplication.java
@@ -0,0 +1,13 @@
+package com.favoritahamburgueria.senac.favoritahamburgueria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavoritahamburgueriaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FavoritahamburgueriaApplication.class, args);
+    }
+
+}
